447a831f9edf9960b1be2b73ddde7aec38199fa2
John O <johnolivas7553@gmail.com>
2022-06-01 09:34:18 -0700

    Platform update
    The spawning platforms have been updated.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -29,10 +29,22 @@ public class PlayerControls : MonoBehaviour{
     public GameObject follower21;
     public GameObject follower22;
     public GameObject follower23;
+    public GameObject follower24;
+    public GameObject follower25;
+    public GameObject follower26;
+    public GameObject follower27;
+    public GameObject follower28;
+    public GameObject follower29;
+    public GameObject follower30;
+    public GameObject follower31;
+    public GameObject follower32;
+    public GameObject follower33;
+    public GameObject follower34;
 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int spawnCount = 0;
     // Start is called before the first frame update
     void Start(){
         rb = GetComponent<Rigidbody>();
@@ -53,14 +65,71 @@ public class PlayerControls : MonoBehaviour{
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Spawn short soldier")){
+        if (other.gameObject.CompareTag("Spawn short soldier") && spawnCount == 0){
             other.gameObject.SetActive(false);
             follower.SetActive(true);
             follower1.SetActive(true);
             follower2.SetActive(true);
-        }
-        if (other.gameObject.CompareTag("spawnSoldier")){
+            follower3.SetActive(true);
             follower4.SetActive(true);
+            spawnCount++;
+        }
+        else if (other.gameObject.CompareTag("Spawn short soldier") && spawnCount == 1) {
+            other.gameObject.SetActive(false);
+            follower5.SetActive(true);
+            follower6.SetActive(true);
+            follower7.SetActive(true);
+            follower8.SetActive(true);
+            follower9.SetActive(true);
+            spawnCount++;
+        }
+        else if (other.gameObject.CompareTag("Spawn short soldier") && spawnCount == 2) {
+            other.gameObject.SetActive(false);
+            follower10.SetActive(true);
+            follower11.SetActive(true);
+            follower12.SetActive(true);
+            follower13.SetActive(true);
+            follower14.SetActive(true);
+            spawnCount++;
+        }
+        else if (other.gameObject.CompareTag("Spawn short soldier") && spawnCount == 3)
+        {
+            other.gameObject.SetActive(false);
+            follower15.SetActive(true);
+            follower16.SetActive(true);
+            follower17.SetActive(true);
+            follower18.SetActive(true);
+            follower19.SetActive(true);
+            spawnCount++;
+        }
+        else if (other.gameObject.CompareTag("Spawn short soldier") && spawnCount == 4)
+        {
+            other.gameObject.SetActive(false);
+            follower20.SetActive(true);
+            follower21.SetActive(true);
+            follower22.SetActive(true);
+            follower23.SetActive(true);
+            follower24.SetActive(true);
+            spawnCount++;
+        }
+        else if (other.gameObject.CompareTag("Spawn short soldier") && spawnCount == 5)
+        {
+            other.gameObject.SetActive(false);
+            follower25.SetActive(true);
+            follower26.SetActive(true);
+            follower27.SetActive(true);
+            follower28.SetActive(true);
+            follower29.SetActive(true);
+            spawnCount++;
+        }
+        else if (other.gameObject.CompareTag("Spawn short soldier") && spawnCount == 6)
+        {
+            other.gameObject.SetActive(false);
+            follower30.SetActive(true);
+            follower31.SetActive(true);
+            follower32.SetActive(true);
+            follower33.SetActive(true);
+            follower34.SetActive(true);
         }
     }
 }
